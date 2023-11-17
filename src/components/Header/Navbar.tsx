118a6d22462b16460920f34d44ae111e7e66a829
mahyab <mahyab79@gmail.com>
2023-11-17 19:25:51 +0330

    edit: navbar sample responsive

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,38 +1,43 @@
 
  
  import {FaAlignCenter} from  "react-icons/fa";
+ import { FaTimes } from "react-icons/fa";
  import { IconContext } from "react-icons";
+import { useState } from "react";
  function Navbar() {
+    const [isNavbarExpaned , setIsNavbarExpanded] = useState(false);
+    function handleNavbarOpened() {
+        setIsNavbarExpanded(!isNavbarExpaned);
+    }
     return(
-        <nav className="navigation flex  relative bg-custom_brown text-black">
+        <nav className="navigation flex justify-start items-center relative bg-custom_brown text-black">
             <a className="brand-name  text-black ml-2 no-underline">
                 Mahya
             </a>
-            <button className="hamburget-icon hidden border-0 h-2/5 p-2 rounded-full cursor-pointer absolute top-1/2 right-1/4   hover:text-custom_cerem">
-                <IconContext.Provider value={{className:"hover:text-custom_blue"}}>
-                <FaAlignCenter />
-
+            <button onClick={handleNavbarOpened} className={`hamburget-icon lg:hidden md:block md:top-0 border-0 h-2/5  rounded-full cursor-pointer absolute top-1/2 right-4   hover:text-custom_cerem`}>
+                <IconContext.Provider value={{className:"hover:text-custom_blue text-2xl" }}>
+               {isNavbarExpaned ? <FaAlignCenter />: <FaTimes/>} 
                 </IconContext.Provider>
             </button>
-            <div className="nanigation-menu ml-auto">
-              
-                <ul className="flex p-0 list-none my-0 mx-4">
+            <div className=" ml-auto">
+                <ul  className={`${isNavbarExpaned ? "opacity-0 invisible cus-navbar-transition" : "opacity-1  visible cus-navbar-transition "} lg:visible  flex-wrap flex lg:flex-row lg:top-auto mx-0 lg:items-center lg:w-auto lg:static lg:bg-custom_brown  p-0 list-none my-0  absolute top-12 left-0 flex-col w-full bg-custom_cerem text-custom_blue border-2 lg:border-0`}>
                 {[
                     ["Products" , "/products"],
                     ["lorem1" , "/lorem1"],
                     ["lorem2" , "/lorem2"],
-                    ["lorem3", "/lorem4"]
+                    ["lorem3", "/lorem4"],
+                    ["lorem5" , "/lorem5"],
+                    
                 ].map(([title , url]) =>(
-                    <li  className="list-none my-0 mx-4 ">
+                    <li key={title}  className={` list-none my-0 mx-4`}>
                         <a href={url} className="no-underline block w-full">{title}</a>
                     </li>
                 ))}
-                    <li className="list-none my-0 mx-4  ">
-                        <button className="p-2 border-custom_blue border bg-custom_blue hover:bg-custom_brown hover:text-custom_blue hover:border-custom_blue hover:border" style={{marginTop:"-6px"}}>Language</button>
+                    <li className="list-none my-3 lg:my-0 mx-4 ">
+                        <button className="p-2 lg:p-1 border-custom_blue lg:text-custom_cerem   border lg:bg-custom_blue bg-custom_brown hover:bg-custom_brown    hover:text-custom_blue hover:border-custom_blue hover:border" style={{marginTop:"-6px"}}>Language</button>
                     </li>
                     
                 </ul>
-
             </div>
             
         </nav>
