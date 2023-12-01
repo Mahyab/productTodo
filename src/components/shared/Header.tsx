0bf03f7cc6a3a55c6627064ee8e6081b9909e409
mahyab <mahyab79@gmail.com>
2023-12-01 11:04:03 +0330

    delete: master footer and header components

--- src/components/shared/Header.tsx
+++
@@ -1,8 +0,0 @@
-import Navbar from "../Header/Navbar"
-export default function Header() {
-    return(
-        <header className="custom_border_radius bg-custom_brown text-custom_cerem p-11  ">
-            <Navbar/>
-        </header>
-    )
-}
