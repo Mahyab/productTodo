0d0ae10226fc6556593425a71fe77abf6e9b9143
mahyab <mahyab79@gmail.com>
2024-01-01 22:36:09 +0330

    implement search product in component

---
+++ src/components/product/ProductSearch.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent } from "react";
+import { useAppDispatch ,  useAppSelector} from "../../hooks/reduxHook";
+import { changeSearchTerm } from "../../store/slices/productsSlice";
+
+function ProductSearch() {
+    const dispatch = useAppDispatch();
+    const searchTerm = useAppSelector((state) => {
+        return state.products.searchTerm
+    })
+    function handleSearchTermChange(event:ChangeEvent<HTMLElement>) {
+        dispatch(changeSearchTerm(event.target.value))
+    }
+    return(
+        <form className="p-4  bg-white flex items-center justify-center border-0 lg:text-end text-center">
+            <input type="text" className="border-custom_cerem border-2"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+            />
+            <button className="bg-custom_brown border-2 border-custom_brown px-3 focus:border-2 !focus:border-custom_cerem  text-custom_cerem">Search</button>
+        </form>
+    )
+}
+export default ProductSearch;
