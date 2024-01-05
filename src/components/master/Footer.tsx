3414d7f3860c74b88fbbbd3085928d3e2652ac29
mahyab <mahyab79@gmail.com>
2024-01-05 20:49:33 +0330

    add footer component

---
+++ src/components/master/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return(
+        <footer className="relative">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#e6cc99" fillOpacity="1" d="M0,0L120,26.7C240,53,480,107,720,106.7C960,107,1200,53,1320,26.7L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path>
+            </svg>
+        </footer>
+    )
+}
