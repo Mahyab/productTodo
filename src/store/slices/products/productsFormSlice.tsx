d00f7e9a705036018525ba626757b2abc95a5f98
mahyab <mahyab79@gmail.com>
2024-01-05 16:36:33 +0330

    change product slices folder

--- src/store/slices/products/productsFormSlice.tsx
+++ src/store/slices/products/productsFormSlice.tsx
@@ -1,5 +1,5 @@
 import { createSlice , PayloadAction } from "@reduxjs/toolkit";
-import { RooteState } from "..";
+import { RooteState } from "../../..";
 import { addProducts } from "./productsSlice";
 
     
--- src/store/slices/products/productsSlice.tsx
+++ src/store/slices/products/productsSlice.tsx
@@ -1,5 +1,5 @@
 import { createSlice  , nanoid, PayloadAction } from "@reduxjs/toolkit";
-import { RooteState } from "..";
+import { RooteState } from "../../..";
 
 type TproductData = {
     id?: string,
