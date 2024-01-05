67dbb58b0739a2aa467fb84c92465a75b77b0d75
mahyab <mahyab79@gmail.com>
2024-01-05 16:53:30 +0330

    import AppDispatch and rootState from new transformed store config

--- src/hooks/reduxHook.ts
+++ src/hooks/reduxHook.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook , useDispatch , useSelector } from "react-redux";
-import type { RooteState, AppDispatch } from "../store";
+import type { RooteState, AppDispatch } from "..";
 
 export const useAppDispatch:() => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RooteState> = useSelector;
