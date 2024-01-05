6eb88d698e0ad1f4a9a3bba7241b1942a838cf6d
mahyab <mahyab79@gmail.com>
2024-01-05 20:47:02 +0330

    refact main component and wrappr router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css';
-import { store } from "./store/index.tsx";
+import { store } from "./index.tsx";
 import { Provider } from 'react-redux';
-
+import { RouterProvider } from 'react-router-dom';
+import { router } from './components/router/router.tsx';
+import Header from './components/master/Header.tsx';
+import Footer from './components/master/Footer.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Header/>
+          <main>
+              <RouterProvider router={router} />
+          </main>
+      <Footer/>
     </Provider>
   </React.StrictMode>,
 )
